0c0520ff3c0c641ad32f13318c7577eb46ddc454
Ckrenzer <crkrenz72@gmail.com>
2021-05-15 13:20:37 -0400

    removed old stuff, replaced it with correct analysis. The project is about complete!

--- Improving Econometrics/Covid Data/scripts/autocorrelation.R
+++
@@ -1,9 +0,0 @@
-# This section checks the dataset for autocorrelation
-
-# Testing the model for autocorrelation using the Durbin-Watson test
-durbinWatsonTest(results8)
-
-
-## Since the p-value is greater than 0.05, we cannot reject the null
-## hypothesis (which states that the variables have NO autocorrelation).
-## We can conclude, therefore, that the variables are NOT autocorrelated.
